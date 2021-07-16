08f35b88e5ac5284b2f342721f7b662e54878dae
Conor <conorwhitley2@hotmail.com>
2021-07-16 15:40:24 +0100

    Synching prognosis paper stuff and notebooks

--- conv.py
+++ conv.py
@@ -7,6 +7,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset
 
+from sklearn.metrics import roc_auc_score, accuracy_score
+
 class SpecConvNet(nn.Module):
 
     def __init__(self):
@@ -15,11 +17,18 @@ class SpecConvNet(nn.Module):
         super().__init__()
 
         self.stack = nn.Sequential(
-                        nn.Conv1d(1, 1, 5),
+                        nn.Conv1d(3, 8, 5),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
+                        nn.MaxPool1d(5),
+                        nn.Conv1d(8, 1, 3),
+                        nn.ReLU(),
+                        nn.Dropout(0.25),
                         nn.MaxPool1d(3),
-                        nn.Conv1d(1, 1, 5),
-                        nn.Dropout(0.5),
-                        nn.Linear(51, 1),
+                        nn.Linear(10, 8),
+                        nn.ReLU(),
+                        nn.Linear(8,2),
+                        nn.Softmax(2)
                         )
 
     def forward(self, x):
@@ -28,12 +37,38 @@ class SpecConvNet(nn.Module):
 
         return seq
 
+class FTIR_Dataset_C(Dataset):
+
+    def __init__(self, dataframe, y_label, transform=None, target_transform=None):
+        self.y = y_label
+        self.X = dataframe
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+
+        # Make X into tensor compatible with CONV1D layers
+        spectra = torch.tensor(self.X[idx,:], dtype=torch.float)#.unsqueeze(0)
+        #spectra = self.X[idx,:]
+        
+        # Make y compatible with binary cross entropy loss
+        label = torch.tensor(self.y[idx], dtype=torch.float).unsqueeze(0)#.unsqueeze(0)
+    
+
+        if self.transform:
+            spectra = self.transform(spectra)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return spectra, label
 
 class FTIR_Dataset(Dataset):
 
     def __init__(self, dataframe, y_label, transform=None, target_transform=None):
-        self.y = dataframe.reset_index()[y_label].values
-        self.X = dataframe.values
+        self.y = y_label
+        self.X = dataframe
         self.transform = transform
         self.target_transform = target_transform
 
@@ -44,9 +79,10 @@ class FTIR_Dataset(Dataset):
 
         # Make X into tensor compatible with CONV1D layers
         spectra = torch.tensor(self.X[idx,:], dtype=torch.float).unsqueeze(-2)
+        #spectra = self.X[idx,:]
         
         # Make y compatible with binary cross entropy loss
-        label = torch.tensor(self.y[idx], dtype=torch.int).unsqueeze(0).unsqueeze(0)
+        label = torch.tensor(self.y[idx], dtype=torch.float).unsqueeze(0)#.unsqueeze(0)
     
 
         if self.transform:
@@ -57,6 +93,7 @@ class FTIR_Dataset(Dataset):
 
 
 def train_loop(dataloader, model, loss_fn, optimizer):
+
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
@@ -70,10 +107,12 @@ def train_loop(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
+            
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
 def test_loop(dataloader, model, loss_fn):
+    
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
@@ -84,6 +123,12 @@ def test_loop(dataloader, model, loss_fn):
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
+            probs = pred.numpy().squeeze()[:,1]
+
+            auc = roc_auc_score(y.numpy().squeeze()[:,1], probs)
+            accuracy = accuracy_score(y.numpy().squeeze()[:,1], (probs>0.5))
+
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(auc)
+    print(f"Test Error: \n Accuracy: {accuracy:>0.1f}%, Avg loss: {test_loss:>8f} \n")
